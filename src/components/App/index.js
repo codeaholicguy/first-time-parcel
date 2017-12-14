37736727ebf97453ef634951fe97ac8f38691d96
Hoang Nhat Nguyen <hoangnn93@gmail.com>
2017-12-14 23:02:50 +0700

    add remote

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react'
 
 export default class App extends Component {
   render() {
-    return <h1>First time parcel</h1>
+    return (
+      <div>
+        <h1>First time parcel</h1>
+        <span>lorem disump</span>
+      </div>
+    )
   }
 }
